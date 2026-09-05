52354176a2ed66f7bc78e1560fafe0d77ad7b866
Noah Lin <nglin34@gmail.com>
2026-09-05 19:24:23 -0400

    global: Enable INCBIN and string literal macros in PORTABLE mode

--- include/global.h
+++ include/global.h
@@ -23,9 +23,13 @@
 #define asm __asm__
 #endif
 
-// IDE support
-#if defined(__APPLE__) || defined(__CYGWIN__) || defined(__INTELLISENSE__)
-// We define these when using certain IDEs to fool preproc
+// IDE & Portable string literal support
+#if defined(PORTABLE) || defined(__APPLE__) || defined(__CYGWIN__) || defined(__INTELLISENSE__)
+#define _(x)        (x)
+#define __(x)       (x)
+#endif
+
+#if (defined(__APPLE__) || defined(__CYGWIN__) || defined(__INTELLISENSE__)) && !defined(PORTABLE)
 #define _(x)        (x)
 #define __(x)       (x)
 #define INCBIN(...) {0}
