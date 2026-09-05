c1691426639158f28b698df536cec2ec7f218714
Noah Lin <nglin34@gmail.com>
2026-09-05 19:14:45 -0400

    platform: Implement audio, link, and engine stubs

---
+++ src/platform/stubs.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include "gba/m4a_internal.h"
+#include "global.h"
+#include "m4a.h"
+#include "link.h"
+#include "link_rfu.h"
+#include "save_failed_screen.h"
+#include "help_system.h"
+#include "load_save.h"
+// M4A Sound Engine Stubs
+void m4aSoundInit(void) {}
+void m4aSoundMain(void) {}
+void m4aSoundVSync(void) {}
+void m4aSoundVSyncOff(void) {}
+void m4aSoundVSyncOn(void) {}
+void m4aSongNumStart(u16 n) { (void)n; }
+void m4aSongNumStartOrChange(u16 n) { (void)n; }
+void m4aSongNumStop(u16 n) { (void)n; }
+void m4aMPlayAllStop(void) {}
+void m4aMPlayContinue(struct MusicPlayerInfo *mplayInfo) { (void)mplayInfo; }
+void m4aMPlayFadeOut(struct MusicPlayerInfo *mplayInfo, u16 speed) { (void)mplayInfo; (void)speed; }
+void m4aMPlayFadeOutTemporarily(struct MusicPlayerInfo *mplayInfo, u16 speed) { (void)mplayInfo; (void)speed; }
+void m4aMPlayFadeIn(struct MusicPlayerInfo *mplayInfo, u16 speed) { (void)mplayInfo; (void)speed; }
+void m4aMPlayImmInit(struct MusicPlayerInfo *mplayInfo) { (void)mplayInfo; }
+void m4aMPlayStop(struct MusicPlayerInfo *mplayInfo) { (void)mplayInfo; }
+
+// High-level Sound Stubs
+void InitMapMusic(void) {}
+void PlayBGM(u16 songNum) { (void)songNum; }
+void StopBGM(void) {}
+void FadeOutBGM(u8 speed) { (void)speed; }
+void FadeInBGM(u8 speed) { (void)speed; }
+void PlaySE(u16 songNum) { (void)songNum; }
+void PlaySE12WithPanning(u16 songNum, s8 pan) { (void)songNum; (void)pan; }
+void PlayFanfare(u16 songNum) { (void)songNum; }
+void FadeOutFanfare(u8 speed) { (void)speed; }
+bool8 IsFanfareTaskInactive(void) { return TRUE; }
+bool8 IsSEPlaying(void) { return FALSE; }
+bool8 IsBGMStopped(void) { return TRUE; }
+void SoundEngine_Init(void) {}
+
+// RFU & Link Hardware Stubs
+void InitRFU(void) {}
+void InitLink(void) {}
+void OpenLink(void) {}
+void CloseLink(void) {}
+bool8 IsLinkTaskFinished(void) { return TRUE; }
+u8 GetLinkPlayerCount(void) { return 1; }
+bool8 IsLinkMaster(void) { return TRUE; }
+void DestroyWirelessStatusIndicatorSprite(void) {}
+
+// Flash Memory Stub
+void CheckForFlashMemory(void)
+{
+    gFlashMemoryPresent = TRUE;
+}
+
+// Save Failed Screen Stub
+bool32 RunSaveFailedScreen(void) { return FALSE; }
+void SetNotInSaveFailedScreen(void) {}
+
+// Decompress / Mon pic stubs
+void DrawSpindaSpots(u16 species, u32 personality, u8 *dest, bool8 isFrontPic) { (void)species; (void)personality; (void)dest; (void)isFrontPic; }
+const struct CompressedSpriteSheet gMonFrontPicTable[1] = {{0}};
+const struct CompressedSpriteSheet gMonBackPicTable[1] = {{0}};
+bool8 RunHelpSystemCallback(void) { return FALSE; }
+
+// SLoop Service Stub
+u8 SetSLoopSvc(void) { return 0; }
+
+// Additional Link & Engine Stubs
+void LinkVSync(void) {}
+void RfuVSync(void) {}
+bool8 HandleLinkConnection(void) { return FALSE; }
+bool8 Overworld_RecvKeysFromLinkIsRunning(void) { return FALSE; }
+bool8 Overworld_SendKeysToLinkIsRunning(void) { return FALSE; }
+bool32 TryReceiveLinkBattleData(void) { return FALSE; }
+void UpdateWirelessStatusIndicatorSprite(void) {}
+void rfu_REQ_stopMode(void) {}
+u16 rfu_waitREQComplete(void) { return 0; }
+u8 gWirelessCommType = 0;
+void Timer3Intr(void) {}
+void SetFlashTimerIntr(bool8 enable) { (void)enable; }
+void MapMusicMain(void) {}
+struct PokemonCrySong gPokemonCrySongs[1] = {{0}};
+struct SoundInfo gSoundInfo = {0};
+void PlayTimeCounter_Update(void) {}
+void SetDefaultFontsPointer(void) {}
+u8 gQuestLogPlaybackState = 0;
+bool8 gHelpSystemEnabled = FALSE;
+
+// Battle BG offsets referenced by scanline_effect.c
+u16 gBattle_BG0_X = 0;
+u16 gBattle_BG0_Y = 0;
+u16 gBattle_BG1_X = 0;
+u16 gBattle_BG1_Y = 0;
+u16 gBattle_BG2_X = 0;
+u16 gBattle_BG2_Y = 0;
+u16 gBattle_BG3_X = 0;
+u16 gBattle_BG3_Y = 0;
+
+void QuestLog_CutPlayback(void) {}
+
+__attribute__((weak)) void CB2_InitCopyrightScreenAfterBootup(void)
+{
+    printf("[Engine] CB2_InitCopyrightScreenAfterBootup called! State = %d\n", gMain.state);
+}
