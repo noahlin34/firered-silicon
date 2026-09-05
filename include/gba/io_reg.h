b04d503a53dc4f4dec1315882e044236c5733b1f
Noah Lin <nglin34@gmail.com>
2026-09-05 18:59:55 -0400

    gba/io_reg: Support PORTABLE REG_BASE array

--- include/gba/io_reg.h
+++ include/gba/io_reg.h
@@ -1,7 +1,12 @@
 #ifndef GUARD_GBA_IO_REG_H
 #define GUARD_GBA_IO_REG_H
 
+#ifndef PORTABLE
 #define REG_BASE 0x4000000 // I/O register base address
+#else
+#include <stdint.h>
+extern uint8_t REG_BASE[0x400];
+#endif
 
 // I/O register offsets
 
