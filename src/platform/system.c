1df0e103daa605bbc74c439b08356fd9fb6b3549
Noah Lin <nglin34@gmail.com>
2026-09-05 19:14:45 -0400

    platform: Add gHeap and SaveBlock globals to system.c

--- src/platform/system.c
+++ src/platform/system.c
@@ -1,13 +1,23 @@
 #include "global.h"
 #include "platform/platform.h"
-
+#include "load_save.h"
 // Simulated GBA hardware memory spaces
 uint8_t REG_BASE[0x400] __attribute__((aligned(4))) = {0};
 uint8_t PLTT_[0x400] __attribute__((aligned(4))) = {0};
 uint8_t VRAM_[0x18000] __attribute__((aligned(4))) = {0};
 uint8_t OAM_[0x400] __attribute__((aligned(4))) = {0};
-uint8_t FLASH_BASE[131072] __attribute__((aligned(4))) = {0};
+uint8_t FLASH_BASE_[131072] __attribute__((aligned(4))) = {0};
 
 uint16_t INTR_CHECK = 0;
 void *INTR_VECTOR = NULL;
 struct SoundInfo *SOUND_INFO_PTR = NULL;
+
+// Game Engine Heap
+uint8_t gHeap[0x1C000] __attribute__((aligned(4))) = {0};
+
+// Game Engine SaveBlocks
+struct SaveBlock2 gSaveBlock2 = {0};
+struct SaveBlock1 gSaveBlock1 = {0};
+struct SaveBlock2 *gSaveBlock2Ptr = &gSaveBlock2;
+struct SaveBlock1 *gSaveBlock1Ptr = &gSaveBlock1;
+bool32 gFlashMemoryPresent = TRUE;
