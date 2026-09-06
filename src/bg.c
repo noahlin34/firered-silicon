303961e083b8ccf8be5cd198415ce51941f7003d
Noah Lin <nglin34@gmail.com>
2026-09-05 23:05:06 -0400

    bg: Fix IsTileMapOutsideWram for 64-bit host pointers

--- src/bg.c
+++ src/bg.c
@@ -1207,9 +1207,15 @@ bool32 IsInvalidBg32(u8 bg)
 
 bool32 IsTileMapOutsideWram(u8 bg)
 {
+#ifndef PORTABLE
     if (sGpuBgConfigs2[bg].tilemap > (void *)IWRAM_END)
         return TRUE;
     if (sGpuBgConfigs2[bg].tilemap == 0x0)
         return TRUE;
     return FALSE;
+#else
+    if (sGpuBgConfigs2[bg].tilemap == NULL)
+        return TRUE;
+    return FALSE;
+#endif
 }
