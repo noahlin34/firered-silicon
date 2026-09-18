93ad8da9eb80d500b27c7d507bf2462715d18b07
Noah Lin <nglin34@gmail.com>
2026-09-17 23:23:55 -0400

    Request the audio subsystem in the top-level SDL_Init

--- src/platform/sdl2.c
+++ src/platform/sdl2.c
@@ -405,7 +405,7 @@ int Platform_Init(int argc, char **argv)
     (void)argc;
     (void)argv;
 
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
     {
         fprintf(stderr, "Failed to initialize SDL2: %s\n", SDL_GetError());
         return -1;
